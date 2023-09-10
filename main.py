7e3e67fdedc05edead375f37a93e8061985d87da
Radu Popescu <124623904+Radwoo@users.noreply.github.com>
2023-09-11 01:25:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,4 @@
-from flask import Flask, render_template, session, copy_current_request_context
-from flask_socketio import SocketIO, emit, disconnect, send
-from threading import Lock
-from flask_socketio import join_room, leave_room
 import random
-import sys
-# async_mode = None
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'secret!'
-# socket_ = SocketIO(app, async_mode=async_mode)
-# thread = None
-# thread_lock = Lock()
-#
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html', async_mode=socket_.async_mode)
-#
-#
-# # TODO Create input form for username and room and try to separate them
-#
-# @socket_.on('join', namespace='/test')
-# def on_join(message):
-#     username = message['username']
-#     room = message['room']
-#     join_room(room)
-#     print("Welcome to room: " + room + " with user " + username)
-#     send(username + ' has entered the room.', to=room)
-#
-#
-# @socket_.on('leave', namespace='/test')
-# def on_leave(message):
-#     username = message['username']
-#     room = message['room']
-#     leave_room(room)
-#     send(username + ' has left the room.', to=room)
-#
-#
-# @socket_.on('my_event', namespace='/test')
-# def test_message(message):
-#     print(message)
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     session['number_of_cards'] = {'r11': 1, 'n11': 1}
-#
-#     emit('my_response',
-#          {'data': message['data'], 'count': session['receive_count'], 'number_of_cards': session['number_of_cards']})
-#
-#
-# @socket_.on('my_broadcast_event', namespace='/test')
-# def test_broadcast_message(message):
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     aux_var = session.get('number_of_cards')
-#     aux_var['r11'] = 0
-#     print(aux_var)
-#     session['number_of_cards'] = aux_var
-#     emit('my_response',
-#          {'data': message['data'], 'count': session['receive_count'], 'number_of_cards': session['number_of_cards']},
-#          broadcast=True, to="stefan")
-#
-#
-# @socket_.on('disconnect_request', namespace='/test')
-# def disconnect_request():
-#     @copy_current_request_context
-#     def can_disconnect():
-#         disconnect()
-#
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     session['number_of_cards'] = session.get('number_of_cards', 600) + 1
-#     emit('my_response',
-#          {'data': 'Disconnected!', 'count': session['receive_count'], 'number_of_cards': session['number_of_cards']},
-#          callback=can_disconnect)
-#
-#
-# if __name__ == '__main__':
-#     socket_.run(app, debug=True, allow_unsafe_werkzeug=True)
-
-# needed variable
 
 players = [{'username': 'Radu', 'hand': []}, {'username': 'Stefan', 'hand': []}, {'username': 'Paul', 'hand': []}, {'username': 'Cristian', 'hand': []}]
 num_players = len(players)
@@ -122,8 +46,7 @@ def game_setup():
 
 def check_quads():
     global players
-
-    # create a dictionary how many times a certain element is in the list
+    
     count_dict = {}
 
     for item in players[current_player]['hand']:
@@ -160,10 +83,12 @@ def new_turn():
         players[current_player]['hand'].remove(card)
         cards_on_table.append(card)
     current_player += 1
+    
     return selected_cards, cards_on_table, current_player, claimed_card
 
 def player_actions():
     global cards_on_table, current_player, players, selected_cards, claimed_card, turn_ended, game_over
+    
     turn_ended = False
     if current_player > len(players) - 1:
         current_player = 0
@@ -212,7 +137,7 @@ def player_actions():
         current_player += 1
         print('Cards on table are:' + '\n' + str(cards_on_table))
         player_actions()
-
+        
     return selected_cards, cards_on_table, current_player
 
 game_setup()
